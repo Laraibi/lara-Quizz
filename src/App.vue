d6a2cac164d1245800a360633968f6e382e9ec99
Laraibi Mehdi <meh.laraibi@gmail.com>
2024-11-14 18:54:52 +0100

    improuve ui

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Question from "./components/Question.vue";
 const apiKey = import.meta.env.VITE_QUIZZ_API_KEY;
 const categories = ref([]);
 const questions = ref([]);
+const questionToShow = ref(-1);
 const userAnswers = ref([]);
 const selectedCategory = ref("");
 const selectedDiff = ref("easy");
@@ -58,6 +59,7 @@ function fetchQuestions() {
       console.error(error);
     });
   userAnswers.value = [];
+  questionToShow.value = 0;
 }
 function fetchCategories() {
   axios
@@ -118,49 +120,109 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="pannel">
-    <div class="diffucly">
-      <label for="easy">Easy</label>
-      <input
-        type="radio"
-        name="diffculty"
-        id="easy"
-        v-model="selectedDiff"
-        value="easy"
-      />
-      <label for="medium">Medium</label>
-      <input
-        type="radio"
-        name="diffculty"
-        id="medium"
-        v-model="selectedDiff"
-        value="medium"
-      />
-      <label for="hard">Hard</label>
-      <input
-        type="radio"
-        name="diffculty"
-        id="hard"
-        v-model="selectedDiff"
-        value="hard"
-      />
+  <div class="container">
+    <h1 class="text-center">Lara-Quizz</h1>
+    <div class="pannel card rounded">
+      <div class="card-body">
+        <h3 class="text-center">Difficulty</h3>
+
+        <div class="diffucly d-flex justify-content-around my-5">
+          <input
+            class="btn-check"
+            type="radio"
+            name="diffculty"
+            id="easy"
+            v-model="selectedDiff"
+            value="easy"
+          />
+          <label for="easy" class="btn btn-outline-info">Easy</label>
+          <input
+            class="btn-check"
+            type="radio"
+            name="diffculty"
+            id="medium"
+            v-model="selectedDiff"
+            value="medium"
+          />
+          <label for="medium" class="btn btn-outline-warning">Medium</label>
+          <input
+            class="btn-check"
+            type="radio"
+            name="diffculty"
+            id="hard"
+            v-model="selectedDiff"
+            value="hard"
+          />
+          <label for="hard" class="btn btn-outline-danger">Hard</label>
+        </div>
+        <h3 class="text-center">Category</h3>
+        <div class="row d-flex justify-content-center">
+          <div class="col-6">
+            <div class="category">
+              <select
+                class="form-select"
+                name="category"
+                id="category"
+                v-model="selectedCategory"
+              >
+                <option
+                  v-for="cat in categories"
+                  :key="cat.id"
+                  :value="cat.name"
+                >
+                  {{ cat.name }}
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="actions row mt-5 d-flex justify-content-center">
+          <button class="col-3 btn btn-success me-5" @click="fetchQuestions">
+            start
+          </button>
+          <button class="col-3 btn btn-success" @click="checkAnswers">
+            check answers
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="category">
-      <select name="category" id="category" v-model="selectedCategory">
-        <option v-for="cat in categories" :key="cat.id" :value="cat.name">
-          {{ cat.name }}
-        </option>
-      </select>
-    </div>
-    <button @click="fetchQuestions">start</button>
-    <button @click="checkAnswers">check answers</button>
-    <div class="questions">
+    <div class="questions"  v-if="questionToShow >= 0">
       <Question
-        v-for="question in questions"
-        :key="question.id"
-        :question="question"
+       
+        :key="questions[questionToShow].id"
+        :question="questions[questionToShow]"
         @userAnswer="updateUserAnswers"
       />
+      <div class="row d-flex justify-content-end">
+        <div class="col-9">
+          <div
+            class="progress"
+            role="progressbar"
+            aria-label="Animated striped example"
+            aria-valuenow="75"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          >
+            <div
+              class="progress-bar progress-bar-striped progress-bar-animated"
+              :style="{width:`${((questionToShow+1)/questions.length)*100}%`}"
+            ></div>
+          </div>
+          <div class="text-center">
+            {{ (questionToShow+1) }}/{{ questions.length }}
+          </div>
+        </div>
+        <div class="col-3">
+          <button
+            @click="
+              questionToShow += questionToShow == questions.length - 1 ? 0 : 1
+            "
+            class="btn btn-info"
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -6,7 +6,7 @@ const props = defineProps(["question"]);
 const emit = defineEmits(["userAnswer"]);
 
 function updateUserAnswer(index, isChecked) {
-  emit("userAnswer", props.question.id, index,isChecked);
+  emit("userAnswer", props.question.id, index, isChecked);
 }
 </script>
 
@@ -16,21 +16,32 @@ function updateUserAnswer(index, isChecked) {
     <ul>
       <div v-for="(answer, index) in question.answers" :key="index">
         <li v-if="answer != null">
-          <input
-            :type="
-              question.multiple_correct_answers == 'true' ? 'checkbox' : 'radio'
-            "
-            :id="`${props.question.id}-answer-${index}`"
-            :name="`${props.question.id}`"
-            @change="(e) => updateUserAnswer(index, e.target.checked)"
-          />
-          <label :for="`${props.question.id}-answer-${index}`">
-            {{ answer }}</label
-          >
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              :type="
+                question.multiple_correct_answers == 'true'
+                  ? 'checkbox'
+                  : 'radio'
+              "
+              :id="`${props.question.id}-answer-${index}`"
+              :name="`${props.question.id}`"
+              @change="(e) => updateUserAnswer(index, e.target.checked)"
+            />
+            <label
+              class="form-check-label"
+              :for="`${props.question.id}-answer-${index}`"
+            >
+              {{ answer }}</label
+            >
+          </div>
         </li>
       </div>
     </ul>
   </div>
 </template>
 <style>
+ul{
+  list-style: none;
+}
 </style>
